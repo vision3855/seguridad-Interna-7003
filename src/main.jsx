9e7f5114c7ca6318792df34cc52464da0f64bcf1
Ralph Guerson Alcide <vision3855@gmail.com>
2026-01-25 15:34:18 -0500

    convenient changes for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-react-app">
       <App />
     </BrowserRouter>
   </StrictMode>,
